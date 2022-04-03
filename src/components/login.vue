e6a00e279124cd021bbe3f5af1723ee17906314e
tw <tw9930942@163.com>
2022-04-04 01:01:25 +0800

    prepare

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,10 +1,40 @@
 <template>
-  <div>
-    哈哈
-  </div>
+  <div id='warp'>
+<el-container>
+  <el-aside width="280px">Aside</el-aside>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+
+  </el-container>
+</el-container>
+</div>
 </template>
 <script>
 export default {
   
 }
 </script>
+<style scoped>
+  .el-header{
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+</style>
+  
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import {Col,Row,Container,Aside,Main,Header} from 'element-ui'
 Vue.config.productionTip = false
-
+Vue.use(Col)
+Vue.use(Row)
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Header)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
